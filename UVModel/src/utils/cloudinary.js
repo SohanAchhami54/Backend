fab2fc6c4a32bcd090f5114063c94ccaee4e3e59
Sohan Ac <sohanachhami655@gmail.com>
2026-05-25 22:22:32 +0545

    feat(image): add logic to upload image on cloudinary

---
+++ UVModel/src/utils/cloudinary.js
@@ -0,0 +1,23 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs from 'fs'
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadonCloudinary=async(filepath)=>{
+    try {
+        if (!filepath) return null 
+        const response=await cloudinary.uploader.upload(filepath,{
+        resource_type:'auto'
+    }) 
+    console.log('Url of the image is:',response.url)
+    return response
+
+    } catch (error) {
+        fs.unlinkSync(filepath) 
+        return null
+    }
+}
+export {uploadonCloudinary}
