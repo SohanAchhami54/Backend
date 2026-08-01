7df2d5399ead3ffb56012470922019d4dcdc1a68
Sohan Ac <sohanachhami655@gmail.com>
2026-08-01 22:58:17 +0545

    refactor(app): seperate express app configuration into app.js

--- UVModel/src/app.js
+++ UVModel/src/app.js
@@ -1,37 +1,27 @@
-import express from 'express' 
-import dotenv from 'dotenv' 
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import { connectDb } from './db/mongodb.js'
-import userRouter from './routes/user.route.js'
-dotenv.config() 
-const app=express() 
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import userRouter from './routes/user.route.js';
 
-const headers={
-    origin:[process.env.FRONTEND_URL],
-    methods:['GET','POST','PUT','DELETE','PATCH'], 
-    credentials:true
-}
-app.use(cors(headers))
-app.use(cookieParser())
-app.use(express.json({limit:'16KB'})) 
-app.use(express.urlencoded({extended:true,limit:'32KB'}))
-app.use(express.static('public'))
+const app = express();
 
-//routes
-app.use('/users/v1/',userRouter)
-app.get('/',(req,res)=>{
-    res.send('Hello from backend')
-    console.log('Hello from backend')
-})
+const corsOptions = {
+  origin: [process.env.FRONTEND_URL],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  credentials: true,
+};
 
-const port=process.env.PORT 
-connectDb()
-.then(()=>{
-  app.listen(port || 8000,()=>{
-    console.log(`Server is running at the port:${port}`)
-})
-})
-.catch((error)=>{
-    console.log('Error occured while running app',error)
-})
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json({ limit: '16KB' }));
+app.use(express.urlencoded({ extended: true, limit: '32KB' }));
+app.use(express.static('public'));
+
+// Routes
+app.use('/users/v1', userRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello from backend');
+});
+
+export default app;
