ef3b7af8a914e284df3afbf6a58247f152e678bc
Sohan Ac <sohanachhami655@gmail.com>
2026-08-03 14:34:47 +0545

    feat(upload): configure multer fields for user registration

--- UVModel/src/routes/user.route.js
+++ UVModel/src/routes/user.route.js
@@ -1,7 +1,11 @@
 import express from 'express' 
 import { userRegister } from '../controller/user.controller.js'
+import { upload } from '../middleware/multer.middleware.js'
 const router=express.Router() 
 
-router.post('/register',userRegister)
+router.post('/register',upload.fields([
+    {name:'avatar',maxCount:1}, 
+    {name:'coverimage',maxCount:1}
+]), userRegister)
 
 export default router
