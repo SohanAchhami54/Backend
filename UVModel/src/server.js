0e2dc6111322ddf6bdfb2a86e311b7f48e50b828
Sohan Ac <sohanachhami655@gmail.com>
2026-08-04 13:44:53 +0545

    refactor(server): reorganize imports and dotenv setup

--- UVModel/src/server.js
+++ UVModel/src/server.js
@@ -1,17 +1,16 @@
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
+dotenv.config()
 import app from './app.js'
-import { connectDb } from './db/mongodb.js';
-
-dotenv.config();
+import { connectDb } from './db/mongodb.js'
 
 const port = process.env.PORT || 8000;
 
 connectDb()
   .then(() => {
     app.listen(port, () => {
-      console.log(`Server is running at port: ${port}`);
-    });
+      console.log(`Server is running at port: ${port}`)
+    })
   })
   .catch((error) => {
-    console.error('Error starting server:', error);
-  });
+    console.error('Error starting server:', error)
+  })
