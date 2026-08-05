20ca3ccab1ee688d254adfd806232c2649896913
Sohan Ac <sohanachhami655@gmail.com>
2026-08-05 20:51:42 +0545

    feat(routes): add authentication routes

--- UVModel/src/routes/user.route.js
+++ UVModel/src/routes/user.route.js
@@ -1,11 +1,15 @@
 import express from 'express' 
-import { userRegister } from '../controller/user.controller.js'
+import { userLogin, userLogout, userRegister } from '../controller/user.controller.js'
 import { upload } from '../middleware/multer.middleware.js'
+import { isProtected } from '../middleware/auth.middleware.js'
 const router=express.Router() 
 
 router.post('/register',upload.fields([
     {name:'avatar',maxCount:1}, 
     {name:'coverimage',maxCount:1}
 ]), userRegister)
+
+router.post('/login',userLogin) 
+router.post('/logout',isProtected,userLogout) 
 
 export default router
