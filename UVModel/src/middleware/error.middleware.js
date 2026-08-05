e63eabe4db09f8bc026cd2e0cf95ab2b1bf447c0
Sohan Ac <sohanachhami655@gmail.com>
2026-08-05 13:19:05 +0545

    feat(middleware): add global error handler

---
+++ UVModel/src/middleware/error.middleware.js
@@ -0,0 +1,12 @@
+const errorMiddleware=(err,req,res,next)=>{
+    const statusCode=err.statusCode || 500  
+    const message=err.message || 'Internal Server error' 
+    
+    return res.status(statusCode).json({
+        success:false, 
+        message, 
+        errors:err.error || []
+    })
+}
+
+export {errorMiddleware}
