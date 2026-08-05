dbffe84e0c74ae06c57c54b8568622b639c0561a
Sohan Ac <sohanachhami655@gmail.com>
2026-08-05 13:20:06 +0545

    feat(app): add error middleware to express app

--- UVModel/src/app.js
+++ UVModel/src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import userRouter from './routes/user.route.js'
+import { errorMiddleware } from './middleware/error.middleware.js'
 const app = express()
 
 const corsOptions = {
@@ -21,7 +22,7 @@ app.use('/api/v1/users', userRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello from backend')
-});
-
+})
+app.use(errorMiddleware)
 export default app;
 
