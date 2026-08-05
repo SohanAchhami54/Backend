1a515812cf47c91dd35452aa75f204c94d9bc78a
Sohan Ac <sohanachhami655@gmail.com>
2026-08-05 20:48:29 +0545

    feat(auth): implement protected route middleware

---
+++ UVModel/src/middleware/auth.middleware.js
@@ -0,0 +1,17 @@
+import { userDetails } from "../services/auth.js";
+import { Apierror } from "../utils/Apierror.js";
+import { Asyncerror } from "../utils/Asyncerror.js";
+import { verifyJwt } from "../utils/auth.js";
+
+const isProtected=Asyncerror(async(req,res,next)=>{
+   const token=req.cookies?.accessToken || req.header('Authorization')?.replace('Bearer ','')  
+   console.log('token value is:',token)
+   if(!token) throw new Apierror(400,'Token not found')
+
+   const decodedToken=verifyJwt(token)  
+   const userData=await userDetails(decodedToken?._id)  
+   req.user=userData 
+   next()
+})
+
+export {isProtected}    
