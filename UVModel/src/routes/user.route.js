71bee62be88add0ba4711db452e1f221ee57ddf6
Sohan Ac <sohanachhami655@gmail.com>
2026-08-07 17:21:54 +0545

    feat(routes): add routes for password change, current user and profile update

--- UVModel/src/routes/user.route.js
+++ UVModel/src/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express' 
-import { userLogin, userLogout, userRegister } from '../controller/user.controller.js'
+import { changePassword, generateNewAccessRefreshToken, getCurrentUser, updateAccountDetails, userLogin, userLogout, userRegister } from '../controller/user.controller.js'
 import { upload } from '../middleware/multer.middleware.js'
 import { isProtected } from '../middleware/auth.middleware.js'
 const router=express.Router() 
@@ -9,7 +9,11 @@ router.post('/register',upload.fields([
     {name:'coverimage',maxCount:1}
 ]), userRegister)
 
-router.post('/login',userLogin) 
+router.post('/login',userLogin)  
+router.post('/reset-token',generateNewAccessRefreshToken)
+router.post('/changepassword',isProtected,changePassword) 
+router.post('/getcurrentuser',isProtected,getCurrentUser) 
+router.patch('/updateuser',isProtected,updateAccountDetails)
 router.post('/logout',isProtected,userLogout) 
 
 export default router
