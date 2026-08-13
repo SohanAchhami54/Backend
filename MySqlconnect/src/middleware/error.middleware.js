d734e295f30b959166359d389bfb10f8f3dbf5c8
Sohan Ac <sohanachhami655@gmail.com>
2026-08-13 12:32:00 +0545

    feat(error): add global error handling middleware

---
+++ MySqlconnect/src/middleware/error.middleware.js
@@ -0,0 +1,9 @@
+const errorMiddleware=(err,req,res,next)=>{
+    const statusCode=err.statusCode || '400' 
+    const message= err.message || 'Internal Server error' 
+    return res.status(statusCode).json({
+        success:'false',
+        message, 
+        errors:err.error || []
+    })
+}
