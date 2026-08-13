a9c7031b958edcf921ab064253bd2f4a447d6476
Sohan Ac <sohanachhami655@gmail.com>
2026-08-13 17:00:41 +0545

    feat(error): add custom apierror class

---
+++ PostgresConnect/src/utils/Apierror.js
@@ -0,0 +1,10 @@
+class Apierror extends Error{
+    constructor(statusCode,message='Something went wrong',error=[],stack){
+       super(message) 
+       this.statusCode=statusCode 
+       this.error=error 
+       this.stack=stack 
+       this.success=false 
+    }
+}
+export {Apierror}
