bfc74012f81c098d2b302d249def3ff919defd2e
Sohan Ac <sohanachhami655@gmail.com>
2026-08-14 16:54:57 +0545

    chore(connectsupabase): remove supabase setup

--- ConnectSupabase/src/server.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv' 
-import { app } from './app.js'
-dotenv.config() 
-const PORT=process.env.PORT || 3002 
-
-app.listen(PORT,()=>{
-    console.log(`Server is running at the port:${PORT}`)
-})
