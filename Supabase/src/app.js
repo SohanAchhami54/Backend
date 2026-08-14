13984b4b6c46888355df965bcbc908517d3aede2
Sohan Ac <sohanachhami655@gmail.com>
2026-08-14 21:35:01 +0545

    feat(app): add user routes and  error middleware

--- Supabase/src/app.js
+++ Supabase/src/app.js
@@ -3,7 +3,8 @@ import dotenv from 'dotenv'
 dotenv.config() 
 import cors from 'cors' 
 import cookieParser from 'cookie-parser' 
-
+import { errorMiddleware } from './middleware/error.middleware.js'
+import userRouter from './routes/user.routes.js'
 const app=express() 
 
 const corsOptions={
@@ -16,5 +17,9 @@ app.use(cors(corsOptions))
 app.use(express.json({limit:'16KB'})) 
 app.use(express.urlencoded({extended:true,limit:'32KB'})) 
 app.use(express.static('public')) 
+
+app.use('/api/v1/users',userRouter)
+
+app.use(errorMiddleware)
 
 export {app}
