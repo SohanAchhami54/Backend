66418da987ee7c1f5f9d168b004c5884eec7b472
Sohan Ac <sohanachhami655@gmail.com>
2026-08-17 15:12:13 +0545

    feat(tweet): add tweet content and owner fields

---
+++ UVModel/src/models/tweet.model.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose" 
+const likeSchema = new mongoose.Schema({
+     content:{
+       type:String,
+       required:true
+     },
+     owner:{
+        type:mongoose.Schema.Types.ObjectId, 
+        ref:'User'
+     }
+},{timestamps:true}) 
+
+export const Like = mongoose.model(Like,likeSchema)
