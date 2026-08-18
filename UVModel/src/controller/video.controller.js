f6e2c75db769e209281eadbcde86b1526602f453
Sohan Ac <sohanachhami655@gmail.com>
2026-08-18 13:58:46 +0545

    feat(video): implement video fetching with search sorting and pagination

--- UVModel/src/controller/video.controller.js
+++ UVModel/src/controller/video.controller.js
@@ -1,17 +1,19 @@
-import { createVideo, findOwnerVideoDetails, updateVideoThumbnail } from "../services/video.js"
+import { createVideo, findOwnerVideoDetails, updateVideoThumbnail, videoByOwnerId } from "../services/video.js"
 import { Apierror } from "../utils/Apierror.js"
 import { Apiresponse } from "../utils/Apiresponse.js"
 import { Asyncerror } from "../utils/Asyncerror.js"
 import { deleteFromCloudinary, uploadonCloudinary, uploadVideoonCloudinary } from "../utils/cloudinary.js"
-
+import fs from 'fs'
 const videoUpload = Asyncerror(async(req,res)=>{ 
     const {title,description} = req.body 
     if(!title || !description){
         throw new Apierror(400,'All field are required')
     }
+
     const videofile = req.files?.videofile?.[0]?.path 
     const thumbnail = req.files?.thumbnail?.[0]?.path
-    if(!videofile || !thumbnail) throw new Apierror(400,'Videofile do not found') 
+   
+    if(!videofile || !thumbnail) throw new Apierror(400,'Videofile or thumbnail do not found') 
     
     const videofileonCloudinary = await uploadVideoonCloudinary(videofile) 
     if(!videofileonCloudinary) throw new Apierror(400,'Video not upload on cloudinary')
@@ -19,7 +21,7 @@ const videoUpload = Asyncerror(async(req,res)=>{
     const thumbnailonCloudinary = await uploadonCloudinary(thumbnail)
     if(!thumbnailonCloudinary) throw new Apierror(400,'Thumbnail not upload on cloudinary')  
  
-
+  
     const owner = req.user._id
 
     const video = await createVideo(videofileonCloudinary,thumbnailonCloudinary,owner,title,description) 
@@ -58,5 +60,38 @@ const updateThumbnail =Asyncerror(async(req,res)=>{
     )
 })
 
+//for user to get all the videos.
+const getAllVideos= Asyncerror(async(req,res)=>{
+  const {page=1,limit=10,query,sortBy,sortType,userId} = req.query 
 
-export {videoUpload,updateThumbnail}
+  const getVideo = await videoByOwnerId(userId)
+  if(getVideo.length===0) throw new Apierror(400,'Cannot fetch the videos')
+
+  let video = getVideo
+  if(query){
+     video = video.filter(video=>
+        video.title.toLowerCase().includes(query.toLowerCase())
+     )
+  }
+ 
+  //sort
+ if(sortBy){
+    video.sort((a,b)=>{
+        if(sortType==='desc'){
+            return a[sortBy] < b[sortBy] ? 1 : -1 
+        } 
+        return a[sortBy] > b[sortBy] ? 1: -1 
+    })
+ }
+
+ //pagination 
+   const skip = (page-1)*limit 
+   video = video.slice(skip,skip + Number(limit))
+
+  return res.status(200)
+  .json(
+    new Apiresponse(200,video,'Video fetched successfully')
+  )
+})
+
+export {videoUpload,updateThumbnail,getAllVideos}
