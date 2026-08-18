1ef3051acf89f19701d1c690a50c4f76580b336e
Sohan Ac <sohanachhami655@gmail.com>
2026-08-18 18:47:38 +0545

    feat(videoroute): add video publish status toggle endpoint

--- UVModel/src/routes/video.route.js
+++ UVModel/src/routes/video.route.js
@@ -1,5 +1,5 @@
 import express from 'express' 
-import {deleteVideo, getAllVideos, getVideoById, updateVideo,videoUpload } from '../controller/video.controller.js'
+import {deleteVideo, getAllVideos, getVideoById, togglePublishStatus, updateVideo,videoUpload } from '../controller/video.controller.js'
 import { isProtected } from '../middleware/auth.middleware.js'
 import { upload } from '../middleware/multer.middleware.js'
 const router= express.Router() 
@@ -12,5 +12,6 @@ router.post('/videoupload',isProtected,upload.fields([
 router.patch('/updatevideo/:videoId',isProtected,upload.single('thumbnail'),updateVideo)
 router.get('/getallvideo',isProtected,getAllVideos)
 router.get('/getonevideo/:videoId',isProtected,getVideoById)
+router.patch('/togglepublishstatus/:videoId',isProtected,togglePublishStatus)
 router.delete('/deletevideo/:videoId',isProtected,deleteVideo)
 export default router
