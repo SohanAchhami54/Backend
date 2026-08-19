5316916bef7d78dbe2dfce7819a07e41b689d3e6
Sohan Ac <sohanachhami655@gmail.com>
2026-08-19 17:07:40 +0545

    feat(comment): add comment update with owner authorization

--- UVModel/src/controller/comment.controller.js
+++ UVModel/src/controller/comment.controller.js
@@ -1,7 +1,7 @@
-import { commentAddedtoVideo, findallcommentforvideo } from "../services/comment.js";
-import { Apierror } from "../utils/Apierror.js";
-import { Apiresponse } from "../utils/Apiresponse.js";
-import { Asyncerror } from "../utils/Asyncerror.js";
+import { commentAddedtoVideo, findallcommentforvideo, findCommentById, updatedComment } from "../services/comment.js"
+import { Apierror } from "../utils/Apierror.js"
+import { Apiresponse } from "../utils/Apiresponse.js"
+import { Asyncerror } from "../utils/Asyncerror.js"
 
 const  getVideoComments = Asyncerror(async(req,res)=>{
     const {videoId} = req.params  
@@ -36,4 +36,27 @@ const addComment = Asyncerror(async(req,res)=>{
     )
 })
 
-export {getVideoComments,addComment}
+const updateCommentcontent = Asyncerror(async(req,res)=>{ 
+    const {commentId} = req.params 
+    if(!commentId) throw new Apierror(400,'Commentid do not found') 
+    
+    const {content} = req.body
+    if(!content) throw new Apierror(400,'Comment not found')
+
+    const commentDetails = await findCommentById(commentId) 
+    if(!commentDetails) throw new Apierror(400,'Comment Details not found')
+   
+    if(req.user._id.toString()!==commentDetails.owner.toString()){
+        throw new Apierror(400,'Unauthorized to update comment')
+    }
+
+    const commentupdate = await updatedComment(commentId, content)
+    if(!commentupdate) throw new Apierror(400,'Comment content is not updated') 
+
+    return res.status(200)
+    .json(
+        new Apiresponse(200,commentupdate,'Comment update successfully')
+    )
+})
+
+export {getVideoComments,addComment,updateCommentcontent}
