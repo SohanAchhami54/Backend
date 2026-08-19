82c0bda7cfbd3b0b994938076d1874d67bbade55
Sohan Ac <sohanachhami655@gmail.com>
2026-08-19 17:08:35 +0545

    feat(comment): add route for updating comment

--- UVModel/src/routes/comment.route.js
+++ UVModel/src/routes/comment.route.js
@@ -1,9 +1,10 @@
 import express from 'express' 
 import { isProtected } from '../middleware/auth.middleware.js'
-import { addComment, getVideoComments } from '../controller/comment.controller.js'
+import { addComment, getVideoComments, updateCommentcontent } from '../controller/comment.controller.js'
 
 const router = express.Router() 
 
 router.get('/getallcomments/:videoId',isProtected,getVideoComments)
-router.post('/addcomment/:videoId',isProtected,addComment)
+router.post('/addcomment/:videoId',isProtected,addComment) 
+router.patch('/updatecomment/:commentId',isProtected,updateCommentcontent)
 export default router
