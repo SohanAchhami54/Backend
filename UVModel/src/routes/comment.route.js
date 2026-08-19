72b3e36c305b104fbd9485a5249e565596c4839e
Sohan Ac <sohanachhami655@gmail.com>
2026-08-19 17:25:40 +0545

    feat(comment): add route for deleting comment

--- UVModel/src/routes/comment.route.js
+++ UVModel/src/routes/comment.route.js
@@ -1,10 +1,11 @@
 import express from 'express' 
 import { isProtected } from '../middleware/auth.middleware.js'
-import { addComment, getVideoComments, updateCommentcontent } from '../controller/comment.controller.js'
+import { addComment, deleteCommentcontent, getVideoComments, updateCommentcontent } from '../controller/comment.controller.js'
 
 const router = express.Router() 
 
 router.get('/getallcomments/:videoId',isProtected,getVideoComments)
 router.post('/addcomment/:videoId',isProtected,addComment) 
 router.patch('/updatecomment/:commentId',isProtected,updateCommentcontent)
+router.delete('/deletecomment/:commentId',isProtected,deleteCommentcontent)
 export default router
