038f601d4209980811af93fffc7f3869b6461bce
Sohan Ac <sohanachhami655@gmail.com>
2026-08-19 14:44:17 +0545

    feat(comment): add routes for creating and fetching comments

---
+++ UVModel/src/routes/comment.route.js
@@ -0,0 +1,9 @@
+import express from 'express' 
+import { isProtected } from '../middleware/auth.middleware.js'
+import { addComment, getVideoComments } from '../controller/comment.controller.js'
+
+const router = express.Router() 
+
+router.get('/getallcomments',isProtected,getVideoComments)
+router.post('/addcomment/:videoId',isProtected,addComment)
+export default router
