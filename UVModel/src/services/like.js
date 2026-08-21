13b32c1525f50f9dcbbae471afb8fa412f5f422e
Sohan Ac <sohanachhami655@gmail.com>
2026-08-21 14:23:59 +0545

    feat(like): implement comment like services

--- UVModel/src/services/like.js
+++ UVModel/src/services/like.js
@@ -1,21 +1,46 @@
 import { Like } from "../models/like.model.js"
 
-const findExistingLike=async(videoId,userId)=>{
+const findExistingVideoLike=async(videoId,userId)=>{
   return Like.findOne({
         video:videoId, 
         likedBy:userId
     })
 }
 
-const  deleteExistingLike= async(likeId)=>{
+const  deleteExistingVideoLike= async(likeId)=>{
      return Like.findByIdAndDelete(likeId)
 }
 
-const createLike= async(videoId,userId)=>{
+const createVideoLike= async(videoId,userId)=>{
  return await Like.create({
     video:videoId, 
     likedBy:userId
   })
 }
 
-export {findExistingLike,deleteExistingLike,createLike}
+const findExistingCommentLike =async(commentId,userId)=>{
+   return Like.findOne({
+     comment:commentId, 
+     likedBy:userId
+   })
+}
+
+const deleteExistingCommentLike= async(likeId)=>{
+   return Like.findByIdAndDelete(likeId)
+}
+
+const createCommentLike = async(commentId,userId)=>{
+   return await Like.create({
+        comment:commentId, 
+        likedBy:userId
+    })
+}
+
+export {
+    findExistingVideoLike,
+    deleteExistingVideoLike,
+    createVideoLike,
+    findExistingCommentLike,
+    deleteExistingCommentLike,
+    createCommentLike
+}
