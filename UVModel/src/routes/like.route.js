551bcd04dfd44f745bc930b8c28156fb9f3467c6
Sohan Ac <sohanachhami655@gmail.com>
2026-08-21 14:51:35 +0545

    feat(like): add toggle tweet like endpoint

--- UVModel/src/routes/like.route.js
+++ UVModel/src/routes/like.route.js
@@ -1,10 +1,10 @@
 import express from 'express' 
 import { isProtected } from '../middleware/auth.middleware.js'
-import { toggleCommentLike, toggleVideoLike } from '../controller/like.controller.js'
+import { toggleCommentLike, toggleTweetLike, toggleVideoLike } from '../controller/like.controller.js'
 
 const router = express.Router() 
 
 router.patch('/togglelikevideo/:videoId',isProtected,toggleVideoLike) 
 router.patch('/togglecommentlike/:commentId',isProtected,toggleCommentLike)
-
+router.patch('/toggletweetlike/:tweetId',isProtected,toggleTweetLike)
 export default router 
