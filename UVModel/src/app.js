a253cac669592cd8718e012999b3925e88727115
Sohan Ac <sohanachhami655@gmail.com>
2026-08-21 13:20:09 +0545

    feat(like): integrate like router into app

--- UVModel/src/app.js
+++ UVModel/src/app.js
@@ -3,7 +3,8 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import userRouter from './routes/user.route.js'  
 import videoRouter from './routes/video.route.js'
-import commentRouter from './routes/comment.route.js'
+import commentRouter from './routes/comment.route.js' 
+import likeRouter from './routes/like.route.js'
 import { errorMiddleware } from './middleware/error.middleware.js'
 const app = express()
 
@@ -22,7 +23,8 @@ app.use(express.static('public'))
 // Routes
 app.use('/api/v1/users', userRouter) 
 app.use('/api/v1/video',videoRouter)
-app.use('/api/v1/comment',commentRouter)
+app.use('/api/v1/comment',commentRouter) 
+app.use('/api/v1/like',likeRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello from backend')
