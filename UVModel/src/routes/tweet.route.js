85e8945f343a1edf6b82df717d1b9a1e96faa0c6
Sohan Ac <sohanachhami655@gmail.com>
2026-08-24 13:25:09 +0545

    feat(tweet): add route to get user tweets

--- UVModel/src/routes/tweet.route.js
+++ UVModel/src/routes/tweet.route.js
@@ -1,8 +1,9 @@
 import express from 'express' 
 import { isProtected } from '../middleware/auth.middleware.js'
-import { createTweet } from '../controller/tweet.controller.js'
+import { createTweet, getUserTweets } from '../controller/tweet.controller.js'
 const router = express.Router() 
 
-router.post('/createtweet',isProtected,createTweet)
+router.post('/createtweet',isProtected,createTweet) 
+    router.get('/getusertweet',isProtected,getUserTweets)
 
 export default router
