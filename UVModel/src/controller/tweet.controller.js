6b7eb11fd239030e6e3bde8357977779b4fb9098
Sohan Ac <sohanachhami655@gmail.com>
2026-08-24 13:24:18 +0545

    feat(tweet): add controller to get user tweets

--- UVModel/src/controller/tweet.controller.js
+++ UVModel/src/controller/tweet.controller.js
@@ -1,4 +1,4 @@
-import { addTweet } from "../services/tweet.js";
+import { addTweet, getallusertweet } from "../services/tweet.js";
 import { Apierror } from "../utils/Apierror.js";
 import { Apiresponse } from "../utils/Apiresponse.js";
 import { Asyncerror } from "../utils/Asyncerror.js";
@@ -16,6 +16,19 @@ const createTweet = Asyncerror(async(req,res)=>{
     )
 })
 
+const getUserTweets = Asyncerror(async(req,res)=>{ 
+    const userId = req.user._id 
+    if(!userId) throw new Apierror(400,'User id do not found') 
+    
+    const alltweet = await getallusertweet(req.user._id)  
+    if(alltweet.length===0) throw new Apierror(400,'User tweet do not found') 
+    
+    return res.status(200)
+    .json(
+        new Apiresponse(200,alltweet,'All user tweet found')
+    )
+})
 
 
-export {createTweet}
+
+export {createTweet,getUserTweets}
