9fe8b3e90a5a25e9ecd5e1bb9cc5b3bf447d725c
Sohan Ac <sohanachhami655@gmail.com>
2026-08-24 11:51:54 +0545

    feat(like): add liked videos endpoint

--- UVModel/src/routes/like.route.js
+++ UVModel/src/routes/like.route.js
@@ -1,10 +1,11 @@
 import express from 'express' 
 import { isProtected } from '../middleware/auth.middleware.js'
-import { toggleCommentLike, toggleTweetLike, toggleVideoLike } from '../controller/like.controller.js'
+import { getLikedVideos, toggleCommentLike, toggleTweetLike, toggleVideoLike } from '../controller/like.controller.js'
 
 const router = express.Router() 
 
 router.patch('/togglelikevideo/:videoId',isProtected,toggleVideoLike) 
 router.patch('/togglecommentlike/:commentId',isProtected,toggleCommentLike)
 router.patch('/toggletweetlike/:tweetId',isProtected,toggleTweetLike)
+router.get('/getlikedvideos',isProtected,getLikedVideos)
 export default router 
