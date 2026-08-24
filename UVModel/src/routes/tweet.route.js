b780dce1c68cb79e73f33ce51c1691af3dfb504a
Sohan Ac <sohanachhami655@gmail.com>
2026-08-24 13:53:54 +0545

    feat(tweet): add route to update tweet

--- UVModel/src/routes/tweet.route.js
+++ UVModel/src/routes/tweet.route.js
@@ -1,9 +1,10 @@
 import express from 'express' 
 import { isProtected } from '../middleware/auth.middleware.js'
-import { createTweet, getUserTweets } from '../controller/tweet.controller.js'
+import { createTweet, getUserTweets, updateTweet } from '../controller/tweet.controller.js'
 const router = express.Router() 
 
 router.post('/createtweet',isProtected,createTweet) 
-    router.get('/getusertweet',isProtected,getUserTweets)
+router.get('/getusertweet',isProtected,getUserTweets) 
+router.patch('/updatetweet/:tweetId',isProtected,updateTweet)
 
 export default router
